9c5ed457430e3f0d2e99acd6b3bb784e6337ae80
lihao <lihao_100@boe.com.cn>
2018-12-29 15:02:42 +0800

    添加fileUtil

--- haoli-sdk-web/src/main/java/com/haoli/sdk/web/util/FileUtil.java
+++ haoli-sdk-web/src/main/java/com/haoli/sdk/web/util/FileUtil.java
@@ -1,5 +1,33 @@
 package com.haoli.sdk.web.util;
 
-public class FileUtil {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+/**
+ * 文件工具类
+ * @author 李昊
+ *
+ */
+public class FileUtil {
+	
+	/**
+	 * 用于读取文件,返回字符串
+	 * @param fileName 文件名，包含文件路径
+	 */
+	public static String readFile(String fileName) throws Exception  {
+		File file = new File(fileName);
+		InputStream fileInputStream = new FileInputStream(file);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream, "utf-8"));
+		StringBuilder fileSb = new StringBuilder();
+		String line = null;
+	    while ((line = br.readLine()) != null) {  
+	    	fileSb.append(line + "\n");  
+	    }
+	    String fileContent = fileSb.toString();
+		br.close();
+		return fileContent;
+	}
 }
