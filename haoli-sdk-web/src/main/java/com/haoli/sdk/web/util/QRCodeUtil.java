177aa326a01f836c9b492589317cb97eb1526e0e
lihao <lihao_100@boe.com.cn>
2019-01-04 18:03:58 +0800

    fix

--- haoli-sdk-web/src/main/java/com/haoli/sdk/web/util/QRCodeUtil.java
+++ haoli-sdk-web/src/main/java/com/haoli/sdk/web/util/QRCodeUtil.java
@@ -1,9 +1,5 @@
 package com.haoli.sdk.web.util;
 
-import java.io.IOException;
-
-import com.google.zxing.WriterException;
-
 /**
  * 二维码工具类
  * @author 10063731
@@ -12,7 +8,9 @@ import com.google.zxing.WriterException;
 public class QRCodeUtil {
 	
 	public static void writeToFile(String content, String path, Integer width,
-			Integer height) throws WriterException, IOException {}
+			Integer height) {
+		
+	}
 
 
 }
