1d8f5f07cf48e515f404ca3883c946928ff52c52
lihao <lihao_100@boe.com.cn>
2019-01-08 10:50:33 +0800

    添加rsa工具类

---
+++ haoli-sdk-web/src/main/java/com/haoli/sdk/web/domain/RSAKey.java
@@ -0,0 +1,53 @@
+package com.haoli.sdk.web.domain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+public class RSAKey {
+	private RSAPrivateKey privateKey;
+	private String privateKeyString;
+	private RSAPublicKey publicKey;
+	public String publicKeyString;
+
+	public RSAKey() {
+	}
+
+	public RSAKey(RSAPrivateKey privateKey, String privateKeyString, RSAPublicKey publicKey, String publicKeyString) {
+		this.privateKey = privateKey;
+		this.privateKeyString = privateKeyString;
+		this.publicKey = publicKey;
+		this.publicKeyString = publicKeyString;
+	}
+
+	public RSAPrivateKey getPrivateKey() {
+		return this.privateKey;
+	}
+
+	public void setPrivateKey(RSAPrivateKey privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public String getPrivateKeyString() {
+		return this.privateKeyString;
+	}
+
+	public void setPrivateKeyString(String privateKeyString) {
+		this.privateKeyString = privateKeyString;
+	}
+
+	public RSAPublicKey getPublicKey() {
+		return this.publicKey;
+	}
+
+	public void setPublicKey(RSAPublicKey publicKey) {
+		this.publicKey = publicKey;
+	}
+
+	public String getPublicKeyString() {
+		return this.publicKeyString;
+	}
+
+	public void setPublicKeyString(String publicKeyString) {
+		this.publicKeyString = publicKeyString;
+	}
+}
---
+++ haoli-sdk-web/src/main/java/com/haoli/sdk/web/util/RsaUtil.java
@@ -0,0 +1,70 @@
+package com.haoli.sdk.web.util;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.haoli.sdk.web.domain.RSAKey;
+
+/**
+ * Rsa加密工具类
+ * @author 李昊
+ */
+public class RsaUtil {
+	
+	public static void main(String[] args) throws Exception {
+		RSAKey keys = RsaUtil.genKeyPair();
+		String str = RsaUtil.encrypt("haoli_personal_site", keys.getPublicKeyString());
+		System.out.println(str);
+		String destr = RsaUtil.decrypt(str, keys.getPrivateKeyString());
+		System.out.println(destr);
+	}
+	
+
+	public static RSAKey genKeyPair() throws NoSuchAlgorithmException {
+		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+		keyPairGen.initialize(1024, new SecureRandom());
+		KeyPair keyPair = keyPairGen.generateKeyPair();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		String publicKeyString = new String(Base64.encodeBase64(publicKey.getEncoded()));
+		String privateKeyString = new String(Base64.encodeBase64(privateKey.getEncoded()));
+		return new RSAKey(privateKey, privateKeyString, publicKey, publicKeyString);
+	}
+
+	public static String encrypt(String source, String publicKey) throws Exception {
+		byte[] decoded = Base64.decodeBase64(publicKey);
+		RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA")
+				.generatePublic(new X509EncodedKeySpec(decoded));
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(1, pubKey);
+		String outStr = Base64.encodeBase64String(cipher.doFinal(source.getBytes("UTF-8")));
+		return outStr;
+	}
+
+	public static Cipher getCipher(String privateKey) throws Exception {
+		byte[] decoded = Base64.decodeBase64(privateKey);
+		RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA")
+				.generatePrivate(new PKCS8EncodedKeySpec(decoded));
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(2, priKey);
+		return cipher;
+	}
+
+	public static String decrypt(String text, String privateKey) throws Exception {
+		Cipher cipher = getCipher(privateKey);
+		byte[] inputByte = Base64.decodeBase64(text.getBytes("UTF-8"));
+		String outStr = new String(cipher.doFinal(inputByte));
+		return outStr;
+	}
+}
