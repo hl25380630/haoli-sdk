4cdb935b8e5a3902bd8ea9a528912361a1c39900
lihao <lihao_100@boe.com.cn>
2019-02-13 13:49:24 +0800

    add merge sort

--- haoli-sdk-algorithem/src/main/java/com/haoli/algorithem/util/SortUtil.java
+++ haoli-sdk-algorithem/src/main/java/com/haoli/algorithem/util/SortUtil.java
@@ -6,32 +6,64 @@ package com.haoli.algorithem.util;
  */
 public class SortUtil {
 	
+	public static void main(String[] args) {
+		SortUtil su = new SortUtil();
+		int[] array = {1, 4, 3, 5, 9, 8, 7, 2, 6};
+		su.mergeSort(array);
+		for(int i=0; i<array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+	}
+	
+	public void mergeSort(int[] array) {
+		this.sort(array, 0, array.length-1);
+	}
+	
 	/**
 	 * merge sort 归并排序
 	 * @param array
 	 */
-	public int[] mergeSort(int[] array) {
-		int start = 0;
-		int end = array.length-1;
-		int mid = (end -start)/2;
-		int[] array1 = this.sort(array, start, mid);
-		int[] array2 = this.sort(array, mid+1, end);
-		int[] result = this.mergeArrays(array1, array2);
-		return result;
-	}
-	
-	public int[] sort(int[] array, int start, int end) {
-		int[] tempArray = new int[array.length];
+	public void sort(int[] array, int start, int end) {
 		if(start >= end) {
-			tempArray[start] = array[start];
-			return tempArray;
+			return;
 		}
-		
-		return null;
+		int mid = (end + start)/2;
+		this.sort(array, start, mid);
+		this.sort(array, mid+1, end);
+		this.mergeArrays(array, start, mid, end);
 	}
 	
-	public int[] mergeArrays(int[] array1, int[] array2) {
-		return null;
+	public void mergeArrays(int[] array, int start, int mid, int end) {
+		int[] tempArray = new int[array.length];
+		int tempIndex = start;
+		int index = start;
+		int halfIndex = mid+1;
+		while(start <= mid && halfIndex <= end) {
+			if(array[start] <= array[halfIndex]) {
+				tempArray[tempIndex] = array[start];
+				tempIndex++;
+				start++;
+			}else {
+				tempArray[tempIndex] = array[halfIndex];
+				tempIndex++;
+				halfIndex++;
+			}
+		}
+		while(start <= mid) {
+			tempArray[tempIndex] = array[start];
+			tempIndex++;
+			start++;
+		}
+		while(halfIndex <=end) {
+			tempArray[tempIndex] = array[halfIndex];
+			tempIndex++;
+			halfIndex++;
+		}
+		while(index <= end) {
+			array[index] = tempArray[index];
+			index++;
+		}
 	}
 
 }
